22787f483cd07050ca6ddc1f087b7b73e3f71095
Matheus Desenvolvedor <matheuswalace56@gamil.com>
2022-06-05 19:23:18 -0300

    parametrosurl

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// ativar links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// ativar links do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  elemento.checked = true;
+  console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
